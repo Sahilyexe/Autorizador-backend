29cf3e33fa71445efa43e89f4a2066b209c2beca
Sahily <sahily.exe@gmail.com>
2023-02-11 10:41:11 -0500

    Se agrega el metodo option con configuración

--- src/handlers/autorizacion.js
+++ src/handlers/autorizacion.js
@@ -1,16 +1,10 @@
 // Create clients and set shared const values outside of the handler.
 
-// Get the DynamoDB table name from environment variables
-const tableName = process.env.SAMPLE_TABLE;
-
-// Create a DocumentClient that represents the query to add an item
-const dynamodb = require('aws-sdk/clients/dynamodb');
-const docClient = new dynamodb.DocumentClient();
 
 /**
  * A simple example includes a HTTP get method to get one item by id from a DynamoDB table.
  */
-exports.getByIdHandler = async (event) => {
+exports.login = async (event) => {
   if (event.httpMethod !== 'GET') {
     throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
   }
